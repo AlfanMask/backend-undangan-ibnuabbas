5f62371fbc68a04716c763247015d672db27992b
AlfanMask <afanh123123@gmail.com>
2024-06-07 11:05:49 +0700

    feat: backend node express for undangan-ibnuabbas

---
+++ dist/server.js
@@ -0,0 +1,43 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const sqlite3_1 = __importDefault(require("sqlite3"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
+// initialize express app and middlewares
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)());
+// create db
+const db = new sqlite3_1.default.Database(path_1.default.join(__dirname, "db.sqlite"));
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS rsvp (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, message TEXT, confirmation TEXT)");
+});
+// routes
+app.get('/rsvps', (req, res) => {
+    db.all("SELECT * FROM rsvp ORDER BY id DESC", (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        }
+        res.json(rows);
+    });
+});
+app.post('/rsvp', (req, res) => {
+    const { name, message, confirmation } = req.body;
+    db.run("INSERT INTO rsvp (name, message, confirmation) VALUES (?, ?, ?)", [name, message, confirmation], function (err) {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        }
+        res.json({ id: this.lastID, name, message, confirmation });
+    });
+});
+// server static files from the frontend
+app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
+// start server
+app.listen(3000, () => {
+    console.log("Server is running");
+});
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from "express";
+import sqlite3 from "sqlite3";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+
+// initialize express app and middlewares
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+// create db
+const db = new sqlite3.Database(path.join(__dirname, "db.sqlite"));
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS rsvp (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, message TEXT, confirmation TEXT)")
+})
+
+// routes
+app.get('/rsvps', (req: Request, res: Response) => {
+    db.all("SELECT * FROM rsvp ORDER BY id DESC", (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message })
+        }
+        res.json(rows);
+    });
+});
+
+app.post('/rsvp', (req: Request, res: Response) => {
+    const { name, message, confirmation } = req.body;
+    db.run("INSERT INTO rsvp (name, message, confirmation) VALUES (?, ?, ?)", [name, message, confirmation], function (err) {
+        if (err) {
+            res.status(500).json({ error: err.message })
+        }
+        res.json({ id: this.lastID, name, message, confirmation });
+    })
+});
+
+// server static files from the frontend
+app.use(express.static(path.join(__dirname, "public")))
+
+// start server
+app.listen(3000, () => {
+    console.log("Server is running");
+})
